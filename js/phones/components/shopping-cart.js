bb2bee200138f3e07bbf4a43215579dd9a312ab0
Volodymyr <vovakrivickiy34@gmail.com>
2019-04-15 21:26:09 +0200

    improved cart

--- js/phones/components/shopping-cart.js
+++ js/phones/components/shopping-cart.js
@@ -7,12 +7,13 @@ export default class ShoppingCart extends Component {
   }
 
   addToCart(name) {
-    this._element.insertAdjacentHTML('beforeend', `<li>${name}</li>`);
+    const list = this._element.querySelector('[data-element="cart-list"]');
+    list.insertAdjacentHTML('beforeend', `<li>${name}</li>`);
   }
   _render() {
     this._element.innerHTML = `
       <p>Shopping Cart</p>
-      <ul>
+      <ul data-element="cart-list">
           <li>Phone 1</li>
           <li>Phone 2</li>
           <li>Phone 3</li>
